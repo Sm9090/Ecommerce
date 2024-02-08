00f32ae3a953d8998303ad7b1d0579214997dc32
Sm9090 <Sameermemon834@gmail.com>
2024-02-09 01:38:06 +0500

    wrap with provider persistGate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import  Router  from './Config/Router';
+import Router from './Config/Router';
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux';
+import {store,persistor} from './store'
 import './App.css';
 
 function App() {
   return (
     <>
-    <Router />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
