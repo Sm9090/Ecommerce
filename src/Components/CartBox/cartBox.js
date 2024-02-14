9b213a2c195a788d3bec4bde89caac442505410f
Sm9090 <Sameermemon834@gmail.com>
2024-02-15 03:50:35 +0500

    child to parent remaning

--- src/Components/CartBox/cartBox.js
+++ src/Components/CartBox/cartBox.js
@@ -1,39 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './cartBox.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeCart } from '../../store/cartSlice'
 
-function CartBox({ itemData, index }) {
+function CartBox({ itemData, index , handlePrice }) {
 
     const { price, productImg, productTitle } = itemData
     const [prodQuantity, setProdQuantity] = useState(1)
-
+    
     const dispatch = useDispatch()
-
+    
     const overAllTax = 10 / 100
     const commission = 10 / 100
     const discount = 10 / 100
-
+    
     let mrp = parseInt(price)
     mrp = mrp + overAllTax * mrp + commission * mrp
     const salePrice = mrp - discount * mrp
-    const saving = mrp - salePrice
 
     const increaseQuantity = () => {
         setProdQuantity(prodQuantity + 1)
     }
-
+    
     const decreaseQuantity = () => {
         if (prodQuantity >= 1) {
             setProdQuantity(prodQuantity - 1)
         }
     }
-
+    const totalPrice = salePrice * prodQuantity
+    const [total ,setTotal] = useState(totalPrice)
+    
     const deleteCartItem = () => {
         dispatch(removeCart(index))
-
-
     }
+    
+    // setTotal(totalPrice)
+    useEffect(()=>{
+        sendDataTooParent()
+    },[])
+
+    const sendDataTooParent=()=>{
+        handlePrice(total)
+    }
+    // setTotal(totalPrice)
     return (
         <div className='cart-prod-container'>
             <div className='cart-prod-imgTitle'>
@@ -49,7 +58,7 @@ function CartBox({ itemData, index }) {
                 <p>{prodQuantity}</p>
                 <button onClick={decreaseQuantity}>-</button>
             </div>
-            <div className='prodPrice'>{salePrice * prodQuantity}</div>
+            <div className='prodPrice'>{totalPrice}</div>
             <button className='deleteBtn' onClick={deleteCartItem}>
                 <img src='https://cdn-icons-png.flaticon.com/512/6861/6861362.png' />
             </button>
--- src/Views/Cart/Cart.js
+++ src/Views/Cart/Cart.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import CartBox from '../../Components/CartBox/cartBox'
 import './Cart.css'
 
 function Cart() {
   const cart = useSelector(state => state.cartReducer.cart)
-//   const [cartData , setCartData] = useState()
-//   setCartData(cart)
-// console.log(cartData)
+  const [priceFromChild , setPriceFromChild] = useState()
+  console.log(cart)
 
   if (cart.length === 0) {
     return <div>
       <h2>Your Cart is Empty</h2>
     </div>
+  }
+
+  const handlePrice =(price) =>{
+    setPriceFromChild(price)
   }
 
   return (
@@ -21,7 +24,7 @@ function Cart() {
       <div className='allCartItems'>
       {cart.map((item , index) => {
         return <div>
-          <CartBox key={index} index={index} itemData={item}/>
+          <CartBox key={index} index={index} itemData={item} handlePrice={handlePrice}/>
         </div>
       })}
       </div>
