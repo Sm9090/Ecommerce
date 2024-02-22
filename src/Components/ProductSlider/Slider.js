9ba477c8babc09f751f4d347805c2cbc1019ceba
Sm9090 <Sameermemon834@gmail.com>
2024-02-23 02:44:42 +0500

    responsive

--- src/Components/ProductSlider/Slider.js
+++ src/Components/ProductSlider/Slider.js
@@ -27,12 +27,16 @@ function Slider(props) {
           breakpoint: { max: 3000, min: 1024 },
           items: 5
         },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
+        laptop: {
+          breakpoint: { max: 1024, min: 800 },
           items: 3
         },
+        tablet: {
+          breakpoint: { max: 800, min: 464 },
+          items: 2
+        },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
+          breakpoint: { max: 600 ,min: 0 },
           items: 1
         }
       };
